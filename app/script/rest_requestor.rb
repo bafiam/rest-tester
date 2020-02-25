37d340ab8ec10ba2a4cf8039b9dec46da893a25a
bafiam <stevebafiam@gmail.com>
2020-02-25 19:48:10 +0300

    add rest run script and user controllers

---
+++ app/script/rest_requestor.rb
@@ -0,0 +1,9 @@
+  # app/script/rest_requestor.rb
+require 'rest-client'
+url = "http://localhost:3000"
+
+#get ruby home page
+# puts RestClient.get(url)
+
+#get user
+puts RestClient.get(url+"/users")
